fcfa2b9896bfa632823f4ddc63da14f8d05c55ea
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-16 08:22:32 +0530

    CheckBit Problem

---
+++ BitManipulation/Ith_Bit_Is_Set_or_Not.py
@@ -0,0 +1,18 @@
+# Problem Statement 
+# Check whethee i th bit is set or not 
+# Explanation : take an number 120 
+#
+# Now using the 8 4 2 1 bit calculating method here -> 128 64  32  16  8  4  2  1 
+#                                                       0   1   1   1  1  0  0  0 
+# Now checking the if the i th bit is set or not        ↑ the i th bit is not set 
+#
+# Below is the python code for the above approach
+#
+def CheckBit(A,i):
+    if(A&(1<<i))==0: return False
+    else : return True
+    
+
+A=120
+i=8                # 8 bit significance 
+print(CheckBit(120,i))
