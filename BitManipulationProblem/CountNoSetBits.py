2380034cff79f9a0d08e5871de833c4d8fc99a73
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-17 15:33:45 +0530

    Unset Ith Bit

--- BitManipulationProblem/CountNoSetBits.py
+++ BitManipulationProblem/CountNoSetBits.py
@@ -4,9 +4,9 @@
 # Output Format --> Return an integer containing how many set bits are there 
 def numSetBits(A):
     binaryformat=format(A,'b')                          # Converting the integer to binary by using format function and storing the value converted into 'binaryformat' variable
-    Count_of_Set_Bits=0                                 # Using an counter named Count_of_Set_bits for counting the set bits 
+    Count_of_Set_Bits=0                                 # Using an counter named Count_of_Set_bits for counting the set bits in binaryformat varaible
     for bit in binaryformat:                            # Executing a loop by using bit as the iterator over the binaryformat variable 
-        if bit=='1':                                        # if i=='1' then we are incrementing Count_of_Set_Bits by 1   
+        if bit=='1':                                    # if bit=='1' then we are incrementing Count_of_Set_Bits by 1   
             Count_of_Set_Bits=Count_of_Set_Bits+1
     return Count_of_Set_Bits                            # After we are returning the Count_of_Set_Bits 
 
