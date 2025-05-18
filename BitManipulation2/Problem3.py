9d3438fcab153c399a4c9a4cfe18414722c29e70
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-18 17:34:33 +0530

    Update Problem3.py

--- BitManipulation2/Problem3.py
+++ BitManipulation2/Problem3.py
@@ -29,17 +29,18 @@
 # Below is the python code for the above problem statement 
 #
 def FindSubListwithOnesInThem(A):
-    Count_Variable_0 = 0
-    Count_Variable_1 = 0
-    N = len(A)
-    for i in range(len(A)):
-        if A[i] == 0:
-            Count_Variable_1 = Count_Variable_1 + 1
-        else:
-            Count_Variable_0 = Count_Variable_0 + (Count_Variable_1 * (Count_Variable_1 + 1)) // 2
-            Count_Variable_1 = 0
-    Count_Variable_0 = Count_Variable_0 + (Count_Variable_1 * (Count_Variable_1 + 1)) // 2
-    Count_Variable_Ans = (N * (N + 1)) // 2 - Count_Variable_0
+    Count_Variable_0 = 0                                                                                                    # Used for reference for counting purpose 
+    Count_Variable_1 = 0                                                                                                    # Used for reference for counting purpose 
+    N = len(A)                
+    for i in range(len(A)):                                                                                                 # Executing the loop from 0 to total length -1                      
+        if A[i] == 0:                                                                                                       # A[i]==0 --> Says that we are accessing every element in the list where A[i]==0 we are incrementing the Count_Variable_1 by 1
+            Count_Variable_1 = Count_Variable_1 + 1    
+        else:                                                                                                               # If not the if conditions fails there else condition will get executed                                    
+            Count_Variable_0 = Count_Variable_0 + (Count_Variable_1 * (Count_Variable_1 + 1)) // 2                          # It A[i]==1 then N*(N+1)//2 will be applied here 
+            Count_Variable_1 = 0                                                                                            # Re_Initialising the Count_Variable_1 with 0 again for calculation of sub list with zero
+    Count_Variable_0 = Count_Variable_0 + (Count_Variable_1 * (Count_Variable_1 + 1)) // 2                                  
+    Count_Variable_Ans = (N * (N + 1)) // 2 - Count_Variable_0                                                              # Subtracting total no of sub list which consist of whole zeroes inthem with the sub list of containing both the zeroes and ones  
+
     return Count_Variable_Ans
 
 
