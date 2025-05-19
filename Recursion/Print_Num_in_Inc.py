93c715f946aa8023237affe86cf5b3b4fe67ee2f
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-19 09:42:28 +0530

    Update Print_Num_in_Inc.py

--- Recursion/Print_Num_in_Inc.py
+++ Recursion/Print_Num_in_Inc.py
@@ -3,7 +3,7 @@
 # You are given an integer A, print 1 to A using using recursion
 # Note : After printing all the numbers from 1 to A, print a new line 
 # Input Format : First argument A is an integer 
-# Output Format : Print A space-separated integers 1 to A. Note There should be exactly one psace after each integer. After printing all the intergers, print a new line 
+# Output Format : Print A space-separated integers 1 to A. Note There should be exactly one psace after each integer. After printing all the intergers, print a new line  
 # A = 10 
 # 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 
 # Explanation : Print 1 to 10 space separated integers
