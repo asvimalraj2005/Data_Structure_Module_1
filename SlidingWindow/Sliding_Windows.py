15bfdd52e8734981a79b62f74a0de82a9b3cf3c7
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-29 10:13:15 +0530

    Sliding_Windows.py

--- SlidingWindow/Sliding_Windows.py
+++ SlidingWindow/Sliding_Windows.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Subarray : Continuoes part of array 
 # 1 2 3 4 5 
 # {2,3}  wrong
@@ -37,40 +37,3 @@ def Subarray(A):
 # and process the sum of all the sub-arrays 
 A=[1,2,3,4,5]
 print(Subarray(A))                                         # The answer is printed here 
-=======
-# Subarray : Continuoes part of array 
-# 1 2 3 4 5 
-# {2,3}  wrong
-# 2 4 1 6 -3 7 8 4 
-# 1 6 8 
-# 1 4 
-# 6 1 4 2 
-# 7 8 4 
-# No of subarrays = N * ( N + 1 ) / 2 
-# 
-# Given an array find the sum of all subarray
-# Example array = {3,2,5}
-#               --> 3
-#               --> 3 2 
-#               --> 3 2 5 
-#               --> 2 
-#               --> 2 5 
-#               --> 5 
-# 
-# Direct approach
-# for each subarray, Iterate and get the sum 
-# Below is the code for the above approach
-def Subarray(A):
-    ans=0
-    for s in range(len(A)):                                # s iteration element travels all the elements in the sub array 
-        for u in range(len(A)):                            # for every s, u travels along with it 
-            sum=0                                          # To process the sum of the sub-array we are using sum variable to store the sum of the sub-array 
-            for b in range(u+1):                           # This loop is used to iterate all the elements in the sub-array 
-                sum=sum+A[b]                               # Summing the elements 
-            ans=ans+sum                                    # After processing the sum of the current sub-array, we are appending the progressed sum to the next sub-array sum calculation 
-    return ans                                             # Returning the overall sum to the main function 
-# But the above approach time complexity is O(n^3)
-# and process the sum of all the sub-arrays 
-A=[1,2,3,4,5]
-print(Subarray(A))                                         # The answer is printed here 
->>>>>>> efaf86c7ee90cea27ac78c43a76e224813039749
