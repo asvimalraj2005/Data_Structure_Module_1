b531e630d16133529f167811f7e1582d459358f8
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-31 08:37:47 +0530

    "Resolved"

--- Strings/String2.py
+++ Strings/String2.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Problem Description
 # Akash likes playing with strings. One day he thought of applying following operations 
 # on the string in the given order 
@@ -34,40 +34,3 @@ print(Solve(A))
 
 
 
-=======
-# Problem Description
-# Akash likes playing with strings. One day he thought of applying following operations 
-# on the string in the given order 
-# Concatenate the string with itself
-# Delete all the uppercase letters 
-# Replace each vowel with '#'
-# You are given a string A of size N consisting of lowercase and uppercase alphabets. Return
-# the resultant string after applying the above operations 
-# Note : 'a','e','i','o','u'
-# Input Format : First argument is a string A of size N
-# Output Format : Return the resultant string 
-# Example explanation : A = "aeiOUz"
-#                     O/P = "###z###z"
-#
-# First concatenate the string with itself so string A becomes "aeiOUzaeiOUz"
-# Delete all the uppercase letters so string becomes "aeizaeiz". Now replace vowel with '#', A becomes "###z###z"
-#
-#
-def Solve(A):
-    A=A+A                                # Concatenating string A with itself
-    result=''                            # Creating an empty string named result 
-    vowels={'a','e','i','o','u'}         # Storing the vowels in an set 
-    for ch in A:                         # Iterating over the A 
-        if ch.isupper():                 # If the iterating value over A is Upper character 
-            continue                     # we are continuing 
-        if ch in vowels:                 # If the value in the A is found to be having any vowel character then we are changing the character from a alphabet to "#"
-            result += '#'
-        else:                            # Else result=result+ch
-            result += ch
-    return result                        # Returning the result 
-A = "aeiOUz"
-print(Solve(A))  
-
-
-
->>>>>>> 74973f2aa4ae02848f1c7dd559706885675d7bec
