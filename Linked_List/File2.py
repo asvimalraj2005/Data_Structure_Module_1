9d1f569366ec9662aaf8712198015596fdc4468e
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-06-01 09:48:00 +0530

    File2.py

---
+++ Linked_List/File2.py
@@ -0,0 +1,11 @@
+# Printing linked list 
+# class Node:
+#       def __init__(self,newData):
+#              self.data=newData
+#              self.next=None
+# def PrintList(head):
+#       while head is not None:
+#               print(head.data,end=" ")           # Accessing the node data by using head.data 
+#               head = head . next                 # Iterating over the nodes 
+
+# The above is used to print the entire linked list while maintaining the order and without modifying the node pointers 
