e85b5003bd04b886881e1146ebb2164d201c052b
Vimal Raj <asvimalraj2005@gmail.com>
2025-06-05 13:12:19 +0530

    Problem 3

--- CodeForcesProblems/Problem13.py
+++ CodeForcesProblems/Problem13.py
@@ -6,30 +6,19 @@
 # Input : The first line contains integers n (1<=n<=100) the number of elements in the array. The second line 
 # contains the space-separated integers from the array. All numbers are non-negative integers strictly less than 2^30
 # Output : Print a single integer the required maximal xor of a segment of consecutive elements 
-# 5                                                  3 
-# 1 2 1 1 2                   8 4 2 1                1 2 7          8 4 2 1 
-# 3                     1     0 0 0 1                7        1     0 0 0 1
-#                       2     0 0 1 0                         2     0 0 1 0
-#                       1     0 0 0 1                         7     0 1 1 1
-#                       1     0 0 0 1                                 1 1 1  -> 7   
-#                       1     0 0 0 1 
-#                       2     0 0 1 0 
-#                                 1 1  -> 3      
+# 5                    
+# 1 2 1 1 2
+# 3 
 # Below is the problem solving approach 
 # 5 is the length and using the length we are creating a for loop of range 5 and by using the input function, the number have been appended to the list 
 def Problem():
-    n=int(input())
-    list_num=[]
-    for i in range(n):
-        list_num.append(int(input()))
-                                                    # Now we got the numbers from the user that are kept in the list_num 
-    # Coverting the decimal number to binary format and storing into new list 
-    bin_list=[]
-    for e in list_num:
-        bin=format(e,'b')
-        bin_list.append(bin)
-    
-    bin_list_2=' '.join(bin_list)
-    
+    n = int(input())                            # length
+    a = list(map(int, input().split()))         # input integers 
+    max_xor = 0                                 # variable used for storing the max xor sum 
+    for i in range(n):                          # loop used to run until reaching the length of the array 
+        xor_sum = 0                             # xor_sum initialised as zero
+        for j in range(i, n):                   # consecutive arrays 
+            xor_sum ^= a[j]                     # XOR-ing the numbers 
+            max_xor = max(max_xor, xor_sum)     # Max of all the XOR in the consecutive arrays 
 
-Problem()
+    print(max_xor)                              # Returning the answer 
