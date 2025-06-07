6b52b140536ec43dff407301fcc23d58af28235a
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-06-07 12:20:19 +0530

    Problem20.py

--- CodeForcesProblems/Problem20.py
+++ CodeForcesProblems/Problem20.py
@@ -10,18 +10,36 @@ def Problem20():
     t=int(input())
     for _ in range(t):
         s=input()
-        count_of_a=0
-        count_of_b=0
+        count_of_ab=0
+        count_of_ba=0
         s_list=list(s)
+        if len(s)==1:                                       # For input like a or b 
+            print(s)
+            continue
         for i in range(len(s_list)-1):
             if s_list[i]=='a' and s_list[i+1]=='b':
-                count_of_a=count_of_a+1
+                count_of_ab=count_of_ab+1
         for j in range(len(s_list)-1):
-            if s_list[i]=='b' and s_list[i+1]=='a':
-                count_of_b=count_of_b+1 
-        
-        print(count_of_a)
-        print(count_of_b)
-        
-        
-Problem20()
+            if s_list[j]=='b' and s_list[j+1]=='a':
+                count_of_ba=count_of_ba+1 
+        if count_of_ab>count_of_ba:
+            Found=False
+            for m in range(len(s_list)):
+                if s_list[m]=='b':
+                    s_list[m]='a'
+                    Found=True
+                    break
+        if count_of_ba>count_of_ab:
+            Found=False
+            for n in range(len(s_list)):
+                if s_list[n]=='a':
+                    s_list[n]='b'
+                    Found=True
+                    break 
+        print(''.join(s_list))  
+             
+            
+Problem20()                                         # This is the login I built on my own 
+
+# This is the logic below from online reference
+
