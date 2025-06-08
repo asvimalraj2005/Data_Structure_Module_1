88d4f6ee6d5ea026dddb6e6461767a5fa6bea968
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-06-08 19:30:24 +0530

    Problem24.py

--- CodeForcesProblems/Problem24.py
+++ CodeForcesProblems/Problem24.py
@@ -1,13 +1,1 @@
-# Max A and Max B multiplication
-# https://codeforces.com/problemset/problem/1631/A
-# Below is the code 
-def Problem24():
-    t=int(input())
-    for _ in range(t):
-        a=list(map(int,input().split()))
-        b=list(map(int,input().split()))
-        a_max_element=max(a)
-        b_max_element=max(b)
-        print(a_max_element*b_max_element)
-    
-Problem24()
+
