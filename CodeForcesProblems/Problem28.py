c20405a41a8e2053f0958609f40d1ae09f2cd39a
Vimal Raj <asvimalraj2005@gmail.com>
2025-06-10 20:59:16 +0530

    Problem28

---
+++ CodeForcesProblems/Problem28.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/1553/A
+# Below is the code for the above problem 
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print((n + 1) // 10)
+
+solve()
+
+
+
