6c9e9dce1bcbcee647791a23b820936804b76d9d
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-06-12 23:29:00 +0530

    Problem 3

--- CodeForcesProblems/Problem29.py
+++ CodeForcesProblems/Problem29.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # https://codeforces.com/problemset/problem/1418/A 
 def solve():
     t = int(input())
@@ -7,3 +8,14 @@ def solve():
         stick_= (total_sticks_needed - 1 + (m - 2)) // (m - 1)
         print(stick_ + m)
 solve()
+=======
+# https://codeforces.com/problemset/problem/1418/A 
+def solve():
+    t = int(input())
+    for _ in range(t):
+        x, y, k = map(int, input().split())
+        total_sticks_needed = k * (y + 1)
+        stick_trades = (total_sticks_needed - 1 + (x - 2)) // (x - 1)
+        print(stick_trades + k)
+solve()
+>>>>>>> 3277c29 (Problem3)
