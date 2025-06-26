712e731bc506734ad04aadfba26f844acca57714
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-06-26 23:47:23 +0530

    Problem7.py

---
+++ Linked_List/Problem7.py
@@ -0,0 +1,30 @@
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def oddEvenList(head: ListNode) -> ListNode:
+    if not head or not head.next:
+        return head
+
+    odd = head
+    even = head.next
+    even_head = even
+
+    while even and even.next:
+        odd.next = even.next
+        odd = odd.next
+
+        even.next = odd.next
+        even = even.next
+
+    odd.next = even_head
+    return head
