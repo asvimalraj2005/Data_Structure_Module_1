4a35dea9e30a734e9ce46363304c5e01059fcab4
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 19:44:03 +0530

    Count_distinct_elements.py

--- Hashing/Count_distinct_elements.py
+++ Hashing/Count_distinct_elements.py
@@ -1,1 +1,9 @@
+Count distinct elementslete Solution is Penalty free now
+Problem Description
+Given an array A of N integers, return the number of unique elements in the array.
 
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        return len(set(A))
