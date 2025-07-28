0641cd8ea89dd3ca2eb56a0becd90d59b81fd2dc
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:10:02 +0530

    Merge_Two_Sorted_Arrays.py

---
+++ Sorting/Merge_Two_Sorted_Arrays.py
@@ -0,0 +1,3 @@
+Given two sorted integer arrays A and B, merge B and A as one sorted array and return it as an output.
+
+Note: A linear time complexity is expected and you should avoid use of any library function.
