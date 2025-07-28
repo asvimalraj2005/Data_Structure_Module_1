b63ef6d4d74004396ac0c3a1d262c140903997dc
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:19:31 +0530

    Partition_Index.py

---
+++ Sorting_2./Partition_Index.py
@@ -0,0 +1,7 @@
+Given an integer array A of length N, considering the last element as pivot p, rearrange the elements such that for all i:
+
+if A[i] < p then it should be present on left side of the partition
+if A[i] > p then it should be present on right side of the partition
+Rearrange the given array as well as return the partition index.
+
+Note: All elements are distinct
