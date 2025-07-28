b25b4684177851dc744f34313bb35da51ec8a7b9
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:25:10 +0530

    Implement_Power_Function.py

---
+++ Maths_Modular_Arithmetic/Implement_Power_Function.py
@@ -0,0 +1,3 @@
+Implement pow(A, B) % C.
+In other words, given A, B and C, Find (AB % C).
+Note: The remainders on division cannot be negative. In other words, make sure the answer you return is non-negative.
