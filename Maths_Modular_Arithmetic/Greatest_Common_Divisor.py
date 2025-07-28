37462ed17dcee436c14960d99b172506be3bea14
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:25:39 +0530

    Greatest_Common_Divisor.py

---
+++ Maths_Modular_Arithmetic/Greatest_Common_Divisor.py
@@ -0,0 +1,7 @@
+Given 2 non-negative integers A and B, find gcd(A, B)
+
+
+
+GCD of 2 integers A and B is defined as the greatest integer 'g' such that 'g' is a divisor of both A and B. Both A and B fit in a 32 bit signed integer.
+
+Note: DO NOT USE LIBRARY FUNCTIONS.
