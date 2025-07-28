fc214ecc6effb8125bfd5f166fcc84eda43eb5f4
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:11:29 +0530

    Smallest_Number.py

---
+++ Sorting/Smallest_Number.py
@@ -0,0 +1,5 @@
+An integer is given to you in the form of an array, with each element being a separate digit.
+
+Find the smallest number (leading zeroes are allowed) that can be formed by rearranging the digits of the given number in an array. Return the smallest number in the form an array.
+
+Note - Do not use any sorting algorithm or library's sort method.
