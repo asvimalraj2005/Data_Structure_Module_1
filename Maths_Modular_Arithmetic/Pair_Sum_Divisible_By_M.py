7f4a508cd817b34b0700cb5c0bc8651c29f12b03
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:26:11 +0530

    Pair_Sum_Divisible_By_M.py

---
+++ Maths_Modular_Arithmetic/Pair_Sum_Divisible_By_M.py
@@ -0,0 +1,5 @@
+Given an array of integers A and an integer B, find and return the number of pairs in A whose sum is divisible by B.
+
+Since the answer may be large, return the answer modulo (109 + 7).
+
+Note: Ensure to handle integer overflow when performing the calculations.
