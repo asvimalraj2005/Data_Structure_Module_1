c422b3e516ed8651e9245ac01bdf77fb032c91d0
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-28 20:11:55 +0530

    Sort_by_Color.py

---
+++ Sorting/Sort_by_Color.py
@@ -0,0 +1,9 @@
+Given an array with N objects colored red, white, or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will represent the colors as,
+
+red -> 0
+white -> 1
+blue -> 2
+
+Note: Using the library sort function is not allowed.
