c645c3de46884090420cac6a15e093b1ba43b128
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-31 22:36:49 +0530

    A_B_and_Modulo.py

--- Maths_Modular_Arithmetic/A_B_and_Modulo.py
+++ Maths_Modular_Arithmetic/A_B_and_Modulo.py
@@ -1,1 +1,10 @@
 Given two integers A and B, find the greatest possible positive integer M, such that A % M = B % M.
+
+def greatest_M(A, B):
+    if A == B:
+        return float('inf')
+    if A > B:
+        return A - B
+    else:
+        return B - A
+
