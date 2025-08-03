bf128af2cada543f65b40cb9825ef9b53c3cca4e
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-08-03 23:06:26 +0530

    Find_Two_Missing_Number.py

---
+++ BitManipulation2/Find_Two_Missing_Number.py
@@ -0,0 +1,3 @@
+Given an array A of length N where all the elements are distinct and are in the range [1, N+2].
+
+Two numbers from the range [1, N+2] are missing from the array A. Find the two missing numbers.
