87d45aad2cf824e4848b565e42029007dba57e5d
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-08-08 22:54:38 +0530

    Problem1.py

---
+++ Hashing_2/Problem1.py
@@ -0,0 +1,10 @@
+Given an array of positive integers A and an integer B, find and return first continuous subarray which adds to B.
+
+
+
+
+
+
+If the answer does not exist return an array with a single integer "-1".
+
+First sub-array means the sub-array for which starting index in minimum.
